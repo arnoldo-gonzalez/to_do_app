95e681261b277ad38b2a59387b170a890cc8cef4
Arnoldo Gonzalez <arnoldogonzalezroman@gmail.com>
2022-07-16 13:29:35 -0400

    return to last version, it not works on mobile, but i will learn about PWA, Push notifications and Service Worker to do it, due of in this moment it works with the Notification API

--- js/worker_notify.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener("message", e => {
-    setTimeout(() => {
-        new Notification(`It's time of ${e.data.name}`, { body: `It's ${e.data.currentTime}, and is time of do the task named "${e.data.name}". I wish you luck doing that`})
-    }, e.data.i * 500)
-})
