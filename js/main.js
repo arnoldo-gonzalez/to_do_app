78c13a815745a87c281c771be3c331d174d5b86f
Arnoldo Gonzalez <arnoldogonzalezroman@gmail.com>
2022-07-15 20:26:31 -0400

    version 1.0, lack more classes on modal window

---
+++ js/main.js
@@ -0,0 +1,50 @@
+import addTask, {modal, taskPlace, normalizeTime, removeTask, enableInterval} from "./tasks_control.js"
+
+const html = document.getElementsByTagName("html")[0]
+const btnTheme = document.getElementById("btn-theme")
+const icons = btnTheme.querySelectorAll("i")
+const form = document.getElementById("task-form")
+const btnsModal = document.querySelectorAll(".modal button")
+const {mode, tasks} = localStorage
+
+window.addEventListener("load", () => {
+    if (mode) {
+	html.classList.toggle("dark-mode")
+	icons.forEach(icon => icon.classList.toggle("visible"))
+    }
+
+    if (tasks) {
+        const tasksObj = JSON.parse(tasks)
+	const tasksFiltered = Object.values(tasksObj).filter(e => e !== null)
+	addTask(tasksFiltered)
+    }
+
+    if (Notification.permission !== "granted") modal.classList.add("visible")
+    else enableInterval()
+})
+
+btnTheme.addEventListener("click", e => {
+    html.classList.toggle("dark-mode")
+    icons.forEach(icon => icon.classList.toggle("visible"))
+    localStorage.getItem("mode") ? localStorage.removeItem("mode") : localStorage.setItem("mode", "dark-mode")
+})
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const data = new FormData(form)
+    const objData = Object.fromEntries(data.entries())
+    objData.time = normalizeTime(objData.time)
+    addTask([objData])
+    form.reset()
+})
+
+taskPlace.addEventListener("click", e => {
+    if (e.target.name === "del-task") removeTask(e.target.id, e.target)
+})
+
+btnsModal.forEach(e => e.addEventListener("click", e => {
+    const action = e.currentTarget.dataset.action
+    if (action === "close") modal.classList.remove("visible")
+    else enableInterval()
+}))
---
+++ js/tasks_control.js
@@ -0,0 +1,78 @@
+const tasks = {}
+const times = []
+const taskPlace = document.getElementById("task-place")
+const modal = document.getElementById("modal-notify")
+let id = 0
+
+export {modal, taskPlace}
+
+export const normalizeTime = (time) => {
+    let [hours, minutes] = time.split(":")
+    let hoursHalf = "AM"
+
+    if (parseInt(hours) > 12) {
+	hours = parseInt(hours) - 12
+	hoursHalf = "PM"
+    }
+
+    if (parseInt(hours) === 12 && parseInt(minutes) > 0) hoursHalf = "PM"
+    if (minutes.length < 2) minutes = "0" + minutes
+
+    return hours + ":" + minutes + hoursHalf
+}
+
+export async function enableInterval() {
+    const permission = await Notification.requestPermission()
+    modal.classList.remove("visible")
+
+    if (permission === "granted") {
+        setInterval(() => {
+	    const currentDate = new Date()
+	    const currentTime = normalizeTime(`${currentDate.getHours()}:${currentDate.getMinutes()}`)
+	    if (times.includes(currentTime)) {
+		const tasksToDo = Object.entries(tasks).filter( ([key, val]) => val !== null).filter( ([key, {time}]) => time === currentTime)
+		tasksToDo.forEach(([key, {name}], i) => {
+		    setTimeout(() => {
+                        new Notification(`It's time of ${name}`, { body: `It's ${currentTime}, and is time of do the task named "${name}". I wish you luck doing that`})
+  		        document.getElementById(key).classList.add("time-to-do")
+		    }, i * 1000)
+		    tasks[key] = null
+		})
+                localStorage.setItem("tasks", JSON.stringify(tasks))
+	    }
+        }, 7000)
+    }
+}
+
+export function removeTask(id, element) {
+    tasks[id] = null
+    taskPlace.removeChild(element.parentNode.parentNode)
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+export default function addTask(data) {
+    data.forEach( ({name, time}) => {
+        const task = document.createElement("div")
+        task.classList.add("taskPlace__task")
+        task.innerHTML = `
+        <div class="taskPlace__container-data">
+            <p class="taskPlace__p"><span>Name: </span><span>${name}</span></p>
+            <p class="taskPlace__p"><span>Time: </span><span>${time}</span></p>
+        </div>
+        <div class="taskPlace__container-btn">
+            <button class="taskPlace__btn" id="${id}" name="del-task">Remove Task</button>
+        </div>
+        `
+        taskPlace.appendChild(task)
+
+        tasks[id] = {
+            name,
+            time,
+        }
+
+        times.push(time)
+
+        id++
+    })
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
